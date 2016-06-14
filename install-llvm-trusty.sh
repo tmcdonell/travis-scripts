98b45ab470e8a15a8baed4c67ec3f513426cadf7
Trevor L. McDonell <tmcdonell@cse.unsw.edu.au>
2016-06-14 14:59:53 +1000

    Update 'install-llvm' script
    We need to install from source if we want to use the shared library, but since this takes a while add it to the cached directories.

--- install-llvm-trusty.sh
+++ install-llvm-trusty.sh
@@ -1,21 +1,54 @@
 #!/bin/bash
 #
-# The LLVM apt repository has been disabled. Until it is re-enabled, download
-# the pre-built LLVM binaries and install locally.
+# The LLVM apt repository has been disabled. Until it is re-enabled, install it
+# locally. Note that the prebuilt binaries don't come with the llvm shared
+# library, which we need for llvm-general.
 #
 # As with the other scripts, you must 'source' this for it to work correctly
 #
 
-export LLVM_HOME=${HOME}/llvm-${LLVM}
+export LLVM_HOME=${HOME}/llvm/${LLVM}
 export PATH=${LLVM_HOME}/bin:${PATH}
 export LD_LIBRARY_PATH=${LLVM_HOME}/lib:${LD_LIBRARY_PATH}
 
-if [ ${LLVM%.*} == 3.4 ]; then
-  EXT="xz"
-else
-  EXT="tar.xz"
+mkdir -p ${LLVM_HOME}
+
+# If llvm is already installed (i.e. retrieved from cache) we are done
+#
+if [ ( -x ${LLVM_HOME}/bin/llc ) -a ( -x ${LLVM_HOME}/bin/opt ) -a ( ${LLVM_BINARY_RELEASE:-1} -o ( -e ${LLVM_HOME}/lib/libLLVM-${LLVM}.so ) ) ]; then
+  return 0
 fi
 
-mkdir -p ${LLVM_HOME}
-travis_retry curl -L "http://llvm.org/releases/${LLVM}/clang+llvm-${LLVM}-x86_64-linux-gnu-ubuntu-14.04.${EXT}" | unxz | tar -x -C ${LLVM_HOME} --strip-components 1
+# Download and install. By default choose the source release. This takes longer
+# to insall the first time, but we will probably need the shared library it
+# provides.
+#
+if [ ${LLVM_BINARY_RELEASE:-0} -ne 0 ]; then
+
+  # Download the binary release, and unpack directly into the final location
+  case ${LLVM} in
+    3.4.*) travis_retry curl -L "http://llvm.org/releases/${LLVM}/clang+llvm-${LLVM}-x86_64-linux-gnu-ubuntu-14.04.xz"     | unxz | tar -x -C ${LLVM_HOME} --strip-components 1 ;;
+    *)     travis_retry curl -L "http://llvm.org/releases/${LLVM}/clang+llvm-${LLVM}-x86_64-linux-gnu-ubuntu-14.04.tar.xz" | unxz | tar -x -C ${LLVM_HOME} --strip-components 1 ;;
+  esac
+
+else
+
+  TMPDIR=$(mktemp -d)
+
+  # Download source distribution
+  case ${LLVM} in
+    3.4.*) travis_retry curl -L "http://llvm.org/releases/${LLVM}/llvm-${LLVM}.src.tar.gz" | gunzip | tar -x -C ${TMPDIR} --strip-components 1 ;;
+    *)     travis_retry curl -L "http://llvm.org/releases/${LLVM}/llvm-${LLVM}.src.tar.xz" | unxz   | tar -x -C ${TMPDIR} --strip-components 1 ;;
+  esac
+
+  # Configure, build, install
+  pushd ${TMPDIR}
+  ./configure --prefix=${LLVM_HOME} --enable-shared --enable-targets=host,x86,x86_64,nvptx
+  make install
+  popd
+
+  # Delete the temporary build directory
+  rm -rf ${TMPDIR}
+
+fi
 
