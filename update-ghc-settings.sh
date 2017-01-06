f533b16c6b296aa1f8b5f041ea97ee0b6a9320ca
Trevor L. McDonell <tmcdonell@cse.unsw.edu.au>
2017-01-06 16:23:45 +1100

    ghc-settings: use full numeric version

--- update-ghc-settings.sh
+++ update-ghc-settings.sh
@@ -3,6 +3,8 @@
 # apt). This is a bit of a hack, but is required for llvm-general (>= 3.5.*).
 #
 if [ $(which gcc-4.8) ] && [ -e stack.yaml ]; then
-  sed -i'' -e 's,/usr/bin/gcc.*",/usr/bin/gcc-4.8",' $(stack path --programs 2>/dev/null | tail -n 1)/ghc-${GHC}/lib/ghc-${GHC}/settings
+  BASE=$(stack path --programs 2>/dev/null | tail -n 1)
+  VER=$(stack exec ghc -- --numeric-version)
+  sed -i'' -e 's,/usr/bin/gcc.*",/usr/bin/gcc-4.8",' ${BASE}/ghc-${VER}/lib/ghc-${VER}/settings
 fi
 
