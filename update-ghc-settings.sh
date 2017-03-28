52fab62d6ae2545c747a3b0bbaf596726e04c83c
Trevor L. McDonell <tmcdonell@cse.unsw.edu.au>
2017-03-28 19:08:34 +1100

    ghc-settings: use gcc-4.9

--- update-ghc-settings.sh
+++ update-ghc-settings.sh
@@ -2,9 +2,9 @@
 # Hint at GHC that a newer/modern version of g++ is available (installed via
 # apt). This is a bit of a hack, but is required for llvm-general (>= 3.5.*).
 #
-if [ $(which gcc-4.8) ] && [ -e stack.yaml ]; then
+if [ $(which gcc-4.9) ] && [ -e stack.yaml ]; then
   BASE=$(stack path --programs 2>/dev/null | tail -n 1)
   VER=$(stack exec ghc -- --numeric-version)
-  sed -i'' -e 's,/usr/bin/gcc.*",/usr/bin/gcc-4.8",' ${BASE}/ghc-${VER}/lib/ghc-${VER}/settings
+  sed -i'' -e 's,/usr/bin/gcc.*",/usr/bin/gcc-4.9",' ${BASE}/ghc-${VER}/lib/ghc-${VER}/settings
 fi
 
