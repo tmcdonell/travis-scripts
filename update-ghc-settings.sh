592bee048b545bc66fc240a73fc4c348f95333e1
Trevor L. McDonell <tmcdonell@cse.unsw.edu.au>
2017-06-22 23:10:45 +1000

    update ghc-settings script
    Use ld.gold if it is available, as it is faster and uses less RAM.

--- update-ghc-settings.sh
+++ update-ghc-settings.sh
@@ -2,7 +2,7 @@
 # Hint at GHC that a newer/modern version of g++ is available (installed via
 # apt). This is a bit of a hack, but is required for llvm-general (>= 3.5.*).
 #
-function update_ghc_settings {
+function update_gcc_setting {
   if [ $(which $1) ] && [ -e stack.yaml ]; then
     BASE=$(stack path --programs 2>/dev/null | tail -n 1)
     VER=$(stack exec ghc -- --numeric-version)
@@ -10,7 +10,17 @@ function update_ghc_settings {
   fi
 }
 
-update_ghc_settings gcc-4.8
-update_ghc_settings gcc-4.9
-update_ghc_settings gcc-5
+function update_ld_setting {
+  if [ $(which $1) ] && [ -e stack.yaml ]; then
+    BASE=$(stack path --programs 2>/dev/null | tail -n 1)
+    VER=$(stack exec ghc -- --numeric-version)
+    sed -i'' -e "s,/usr/bin/ld.*\",/usr/bin/$1\"," ${BASE}/ghc-${VER}/lib/ghc-${VER}/settings
+  fi
+}
+
+update_gcc_setting gcc-4.8
+update_gcc_setting gcc-4.9
+update_gcc_setting gcc-5
+
+update_ld_setting  ld.gold
 
