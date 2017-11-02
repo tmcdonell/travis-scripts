cad952b7d62aaf006b5f83a0d7a3127e758dc9cd
Trevor L. McDonell <tmcdonell@cse.unsw.edu.au>
2017-11-02 15:14:52 +1100

    cuda: optionally install extra libraries

--- install-cuda-trusty.sh
+++ install-cuda-trusty.sh
@@ -12,7 +12,10 @@ travis_retry sudo dpkg -i cuda-repo-ubuntu1404_${CUDA}_amd64.deb
 travis_retry sudo apt-get update -qq
 export CUDA_APT=${CUDA:0:3}
 export CUDA_APT=${CUDA_APT/./-}
-travis_retry sudo apt-get install -y cuda-drivers cuda-core-${CUDA_APT} cuda-cudart-dev-${CUDA_APT} cuda-cufft-dev-${CUDA_APT} cuda-cublas-dev-${CUDA_APT} cuda-cusparse-dev-${CUDA_APT} cuda-cusolver-dev-${CUDA_APT}
+travis_retry sudo apt-get install -y cuda-drivers cuda-core-${CUDA_APT} cuda-cudart-dev-${CUDA_APT}
+if [ ${CUDA_INSTALL_EXTRA_LIBS:-1} -ne 0 ]; then
+  travis_retry sudo apt-get install -y cuda-cufft-dev-${CUDA_APT} cuda-cublas-dev-${CUDA_APT} cuda-cusparse-dev-${CUDA_APT} cuda-cusolver-dev-${CUDA_APT}
+fi
 travis_retry sudo apt-get clean
 export CUDA_HOME=/usr/local/cuda-${CUDA:0:3}
 export LD_LIBRARY_PATH=${CUDA_HOME}/nvvm/lib64:${LD_LIBRARY_PATH}
