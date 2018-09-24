b66ce86e90be433097acf807517fd7647f9b099b
Trevor L. McDonell <tmcdonell@cse.unsw.edu.au>
2018-09-24 17:21:09 +0200

    cuda: update to support cuda-10

--- install-cuda-trusty.sh
+++ install-cuda-trusty.sh
@@ -7,17 +7,18 @@
 # 'source' this script, rather than executing it in a sub-process.
 #
 
-travis_retry wget http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1404/x86_64/cuda-repo-ubuntu1404_${CUDA}_amd64.deb
+travis_retry wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1404/x86_64/cuda-repo-ubuntu1404_${CUDA}_amd64.deb
+travis_retry sudo apt-key adv --fetch-keys http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/7fa2af80.pub
 travis_retry sudo dpkg -i cuda-repo-ubuntu1404_${CUDA}_amd64.deb
 travis_retry sudo apt-get update -qq
-export CUDA_APT=${CUDA:0:3}
-export CUDA_APT=${CUDA_APT/./-}
+export CUDA_VER=$(expr ${CUDA} : '\([0-9]*\.[0-9]*\)')
+export CUDA_APT=${CUDA_VER/./-}
 travis_retry sudo apt-get install -y cuda-drivers cuda-core-${CUDA_APT} cuda-cudart-dev-${CUDA_APT}
 if [ ${CUDA_INSTALL_EXTRA_LIBS:-1} -ne 0 ]; then
   travis_retry sudo apt-get install -y cuda-cufft-dev-${CUDA_APT} cuda-cublas-dev-${CUDA_APT} cuda-cusparse-dev-${CUDA_APT} cuda-cusolver-dev-${CUDA_APT}
 fi
 travis_retry sudo apt-get clean
-export CUDA_HOME=/usr/local/cuda-${CUDA:0:3}
+export CUDA_HOME=/usr/local/cuda-${CUDA_VER}
 export LD_LIBRARY_PATH=${CUDA_HOME}/nvvm/lib64:${LD_LIBRARY_PATH}
 export LD_LIBRARY_PATH=${CUDA_HOME}/lib64:${LD_LIBRARY_PATH}
 export PATH=${CUDA_HOME}/bin:${PATH}
